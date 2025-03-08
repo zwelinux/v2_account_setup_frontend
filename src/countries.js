7bb4a15a5a83c1148f5bae90e7b28b20e3692d90
zwelinux <zwelinux@gmail.com>
2025-03-08 15:49:26 +0700

    province update

--- src/countries.js
+++ src/countries.js
@@ -1,34 +1,56 @@
 // src/countries.js
 const countries = [
-    {
-      code: "MM",
-      name: "Myanmar",
-      provinces: [
-        {
-          name: "Yangon Region",
-          cities: ["Yangon", "Insein", "Hlegu"]
-        },
-        {
-          name: "Mandalay Region",
-          cities: ["Mandalay", "Pyin Oo Lwin", "Amarapura"]
-        }
-      ]
-    },
-    {
-      code: "TH",
-      name: "Thailand",
-      provinces: [
-        {
-          name: "Bangkok",
-          cities: ["Bangkok", "Nonthaburi", "Pathum Thani"]
-        },
-        {
-          name: "Chiang Mai Province",
-          cities: ["Chiang Mai", "Lampang", "Lamphun"]
-        }
-      ]
-    }
-  ];
-  
-  export default countries;
-  
+  {
+    code: "MM",
+    name: "Myanmar",
+    provinces: [
+      {
+        name: "Yangon Region",
+        cities: ["Yangon", "Insein", "Hlegu"]
+      },
+      {
+        name: "Mandalay Region",
+        cities: ["Mandalay", "Pyin Oo Lwin", "Amarapura"]
+      }
+    ]
+  },
+  {
+    code: "TH",
+    name: "Thailand",
+    provinces: [
+      {
+        name: "Bangkok",
+        cities: [
+          "Bangkok",
+          "Nonthaburi",
+          "Pathum Thani",
+          "Bang Sue",
+          "Chatuchak",
+          "Phra Nakhon",
+          "Sathon",
+          "Bang Rak",
+          "Dusit",
+          "Thon Buri"
+        ]
+      },
+      {
+        name: "Chiang Mai Province",
+        cities: ["Chiang Mai", "Lampang", "Lamphun"]
+      },
+      {
+        name: "Phuket Province",
+        cities: ["Phuket", "Patong", "Kathu", "Surin"]
+      },
+      {
+        name: "Chonburi Province",
+        cities: ["Pattaya", "Laem Chabang", "Sattahip"]
+      },
+      {
+        name: "Nakhon Ratchasima Province",
+        cities: ["Nakhon Ratchasima", "Korat", "Bua Yai"]
+      }
+    ]
+  }
+];
+
+export default countries;
