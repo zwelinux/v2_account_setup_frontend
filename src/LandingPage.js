d98b6729e3b7b90dbd4289acc54648b80317927d
zwelinux <zwelinux@gmail.com>
2025-03-27 02:56:40 +0700

    27th march 2025 final update

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -17,6 +17,16 @@ const slides = [
     image: "https://images.unsplash.com/photo-1525562723836-dca67a71d5f1?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Express Yourself for Less",
     subtitle: "Trendy pieces. Affordable prices. Ethical impact."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1713520150897-03dcada01520?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Express Yourself for Less",
+    subtitle: "Give pre-loved fashion a second chance to shine"
+  },
+  {
+    image: "https://images.unsplash.com/photo-1699120490830-b944cd507696?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Suit Yourself for Less",
+    subtitle: "Trendy pieces. Affordable prices. Ethical impact."
   }
 ];
 
